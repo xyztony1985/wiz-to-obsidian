0c633cbad5b26068eac4890406fb2d4d99f9295c
Tony <xyztony1985@163.com>
2024-11-21 20:00:52 +0800

    1、html2text转md效果一般，改用markdownify。 2、检测html文件编码失败时，就用utf-i编码。

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,5 @@
 import chardet
-import html2text
+from markdownify import markdownify as md
 from pathlib import Path
 
 
@@ -8,7 +8,7 @@ def html2md(html_file: Path):
     把指定的html文件转换为 Markdown 格式的文本
     """
     html_content = html_file2str(html_file)
-    return html2text.html2text(html_content)
+    return md(html_content)
 
 
 def html_file2str(html_file: Path):
@@ -18,4 +18,4 @@ def html_file2str(html_file: Path):
     html_body_bytes = html_file.read_bytes()
     # 早期版本的 html 文件使用的是 UTF-16 LE(BOM) 编码保存。最新的文件是使用 UTF-8(BOM) 编码保存。要判断编码进行解析
     enc = chardet.detect(html_body_bytes)
-    return html_body_bytes.decode(encoding=enc['encoding'])
+    return html_body_bytes.decode(encoding=enc['encoding'] or 'utf-8')
